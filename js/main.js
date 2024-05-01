087930a43320ee5d5fa917a70f60dcf99db4b1c2
Armaan Pasha <armaanpasha3@gmail.com>
2024-05-01 22:57:21 +0530

    bug fix

--- js/main.js
+++ js/main.js
@@ -2,18 +2,53 @@ var audio;
 
 //Hide Pause Initially
 $('#pause').hide();
+
+let songIndex = 0;
 	
+
+const songList = [
+    {
+        songname: "Yellow Paper Daisy",
+        song: "When Chai Met Toast - Yellow Paper Daisy.mp3",
+        artist: "When Chai Met Toast",
+        cover: "https://i.scdn.co/image/ab67616d00001e02fc3b86df0eb110c1f0eec971",
+        songurl: "https://open.spotify.com/album/13ATYLc7i4HuNbQrAEawOH",
+        artisturl: "https://open.spotify.com/artist/7nZJz6f1b5gJ3jJY0b9xvN"
+    },
+    {
+        songname: "Car's Outside",
+        song: "James Arthur - Car-'s Outside.mp3",
+        artist: "James Arthur",
+        cover: "https://i.scdn.co/image/ab67616d00001e02dc16d839ab77c64bdbeb3660",
+        songurl: "https://open.spotify.com/album/0cm9LOQUBK3JUgyoRj238z",
+        artisturl: "https://open.spotify.com/artist/4IWBUUAFIplrNtaOHcJPRM"
+    },
+    {
+        songname: "Daylight",
+        song: "David Kushner - Daylight.mp3",
+        artist: "David Kushner",
+        cover: "https://i.scdn.co/image/ab67616d00001e0295ca6a9b4083a86c149934ae",
+        songurl: "https://open.spotify.com/album/6NcI39WPu4kY6Tul11nhSv",
+        artisturl: "https://open.spotify.com/artist/33NVpKoXjItPwUJTMZIOiY"
+    },
+    {
+        songname: "to my future wife",
+        song: "normal the kid - to my future wife..mp3",
+        artist: "normal the kid",
+        cover: "https://i.scdn.co/image/ab67616d00001e023636aa34bdf3a18bb69af514",
+        songurl: "https://open.spotify.com/album/7AOX1UCmyhgyNmBu9NmB4f",
+        artisturl: "https://open.spotify.com/artist/3qPVBAEhS0Rc09oB4O065V"
+    }
+]
+
 //Initializer - Play First Song
-initAudio($('#playlist li:first-child'));
+initAudio(songIndex);
 	
-function initAudio(element){
-	var song = element.attr('song');
-    var title = element.text();
-    var cover = element.attr('cover');
-    var artist = element.attr('artist');
-    var songname = element.attr('songname');
-    var songurl = element.attr('songurl');
-    var artisturl = element.attr('artisturl');
+function initAudio(songIndex){
+
+	let songObj = songList[songIndex];
+
+	let {songname, song, artist, cover, songurl, artisturl} = songObj;
 
 	//Create a New Audio Object
 	audio = new Audio('/media/' + song);
@@ -37,15 +72,15 @@ function initAudio(element){
 	//Insert Cover Image
 	document.getElementById('cover').src = cover;
 	
-	$('#playlist li').removeClass('active');
-    element.addClass('active');
+	// $('#playlist li').removeClass('active');
+    // element.addClass('active');
 }
 
 
 //Playlist Song Click
 $('#playlist li').click(function () {
     audio.pause();
-    initAudio($(this));
+    initAudio(this.getAttribute('songIndex'));
 	$('#play').hide();
 	$('#pause').show();
 	$('#duration').fadeIn(400);
@@ -80,24 +115,21 @@ $('#stop').click(function(){
 
 //Next Button
 $('#next').click(function(){
+	console.log(songIndex)
+	if(songIndex == songList.length - 1) {songIndex = 0};
+	songIndex++;
     audio.pause();
-    var next = $('#playlist li.active').next();
-    if (next.length == 0) {
-        next = $('#playlist li:first-child');
-    }
-    initAudio(next);
+    initAudio(songIndex);
 	audio.play();
 	showDuration();
 });
 
 //Prev Button
 $('#prev').click(function(){
+	console.log(songIndex)
+	if(songIndex == 0) songIndex = 3;
     audio.pause();
-    var prev = $('#playlist li.active').prev();
-    if (prev.length == 0) {
-        prev = $('#playlist li:last-child');
-    }
-    initAudio(prev);
+    initAudio(songIndex);
 	audio.play();
 	showDuration();
 });
